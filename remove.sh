073015b0c12ad582af7c36bbcf6ebe9c14a9b36d
xrscode <xrs@icloud.com>
2024-07-23 20:11:15 +0100

    Added login

--- remove.sh
+++ remove.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# File to be deleted
+FILE_PATH="./terraform/terraform.tfvars"
+
+# Check if the file exists before attempting to delete it
+if [ -f "$FILE_PATH" ]; then
+    rm "$FILE_PATH"
+    echo "$FILE_PATH has been deleted."
+    echo "Removing infrastructure now..."
+else
+    echo "$FILE_PATH does not exist."
+fi
+
 # Destroy Infrastructure:
 cd terraform && terraform destroy -auto-approve
---
+++ src/notebooks/transformation/2.driver_standings.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC **Produce Driver Standings**
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Read data into dataframe**
+
+# COMMAND ----------
+
+race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum, when, count, col
+
+driver_standings_df = race_results_df \
+    .groupBy("race_year", "driver_name", "driver_nationality", "team") \
+    .agg(sum("points").alias("total_points"),
+         count(when(col("position") == 1, True)).alias("wins"))
+
+display(driver_standings_df.filter("race_year = 2020"))
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import desc, rank
+
+driver_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
+
+display(final_df.filter("race_year = 2020"))
+
+# COMMAND ----------
+final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/driver_standings")
+
+# COMMAND ----------
+display(spark.read.parquet(f"{presentation_folder_path}/driver_standings"))
